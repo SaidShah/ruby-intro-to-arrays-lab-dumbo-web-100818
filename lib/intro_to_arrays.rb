02f8754a09a3fe88baf0af3e28626c60b93433b8
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-15 21:52:34 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,3 +6,16 @@ def array_with_two_elements
   new_array=["one","two"]
 end
 
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
+end
+
